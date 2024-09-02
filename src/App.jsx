e91b4248f6861cb67cc9d94326282fb8638cc14a
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2024-09-03 05:16:05 +0530

    sdfs

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import PrivacyPolicy from './Components/Razorpay/PrivacyPolicy';
 import ShippingDelivery from './Components/Razorpay/ShippingDelivery';
 import TermsConditions from './Components/Razorpay/TermsConditions';
 import PaymentPage from './Components/PaymentPage';
+import Prescription from './Components/PDF/Prescription';
 // import VideoCall from './Components/VideoCall';
 
 function App() {
