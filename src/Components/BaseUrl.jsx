e8b401ec9b0756ec7526fbcf7d27f54c8d9d365a
Vatsal Rishabh <108565564+vatsalrishabh@users.noreply.github.com>
2024-11-03 21:52:50 +0530

    Update BaseUrl.jsx

--- src/Components/BaseUrl.jsx
+++ src/Components/BaseUrl.jsx
@@ -1,3 +1,3 @@
 // config.js
-export const BaseUrl = "http://pulsecare-backend.onrender.com";
+export const BaseUrl = "https://pulsecare-backend.onrender.com";
 // export const BaseUrl = "http://localhost:3000";
