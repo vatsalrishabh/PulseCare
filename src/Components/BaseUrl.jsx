c00d5c90c89109f756a9352b85ddd9362c90d8e1
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2024-11-03 08:24:53 +0530

    sdf

--- src/Components/BaseUrl.jsx
+++ src/Components/BaseUrl.jsx
@@ -1,3 +1,8 @@
 // config.js
-export const BaseUrl = "http://pulsecare-backend.onrender.com";
+export const BaseUrl = "https://pulsecare-backend.onrender.com";
 // export const BaseUrl = "http://localhost:3000";
+
+// config.js
+// export const BaseUrl = "http://pulsecare-backend.onrender.com";
+// 
+
